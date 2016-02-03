fdc2a5e4679a703b0190c5f758ca362099cf12a7
Landon Powell <landonpowell@linuxmail.org>
2016-02-02 21:18:09 -0500

    Create DeviousYarn.c

---
+++ DeviousYarn.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    if(argc == 2) {
+        FILE* file = fopen(argv[1], "r");
+        if(!file) printf("ERR: %s was not opened. \n", argv[1]);
+        else      printf("%s opened successfully! \n", argv[1]);
+    }
+}
