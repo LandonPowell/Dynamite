8cbd256e3750a88f40b4d4b6a96ff22098359043
Landon Powell <landonjpowell@airmail.cc>
2016-03-25 07:07:03 -0400

    Update DeviousYarn.f90

--- DeviousYarn.f90
+++ DeviousYarn.f90
@@ -1,12 +1,24 @@
-program DeviousYarn
-    character(2000000) :: input
-    do while (.NOT. input .EQ. "KILL")
+subroutine tokenizer(stringInput, tokenList)
+    character, intent(in)  :: stringInput        ! Input
+    character, intent(out) :: tokenList(2000000) ! Output
 
-        print*, "-input-"
+    print *, "Doing Function"
+end subroutine
+
+program DeviousYarn
+
+    character(2000000) :: input !2MB for input.
+    do while (input /= "KILL")
+
+        print *, "-input-"
         read    (*, "(A)") input
 
-        print*, "-output-"
+        call tokenizer(input, input)
+
+        print *, "-output-"
         write   (*, "(A)") trim( input )
 
     end do
+
+    print *, "Thanks for using DeviousYarn~!"
 end program DeviousYarn
