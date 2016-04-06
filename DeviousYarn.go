4f5d4314c0f12147ec234e3d6daae5adb90cd50a
Landon Powell <landonjpowell@airmail.cc>
2016-04-06 12:22:27 -0400

    Created the tokenizer and began work on the nester.

--- DeviousYarn.go
+++ DeviousYarn.go
@@ -1,23 +1,39 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
     "os"
+    "fmt"
+    "bufio"
+    "regexp"
 )
+
+func tokenizer(plaintext string) [][]string {
+    strings := "'[^']+'"
+    brackets:= "[\\[\\](){}]"
+    names   := "[\\w:-@^-`~\\|*-/!-&]+"
+
+    tokens  := regexp.MustCompile(strings+"|"+brackets+"|"+names)
+    return tokens.FindAllStringSubmatch(plaintext, -1)
+}
+
+func nester(tokenList [][]string) {
+}
 
 func main() {
 
     reader := bufio.NewReader(os.Stdin)
-    input  := ""
 
-    for input != "KILL" {
+    var input string
+    for input != "KILL\n" {
+
         fmt.Println(" -input- ")
         input, _ := reader.ReadString('\n')
 
         fmt.Println(" -output- ")
-        fmt.Println(input)
+        fmt.Println( tokenizer( input ) )
+
     }
 
     fmt.Println(" Thanks for using DeviousYarn~! ")
 }
+
