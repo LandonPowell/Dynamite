56ef5efbef1e0cc2c80be7e5d94c547b7582b3d4
Landon Powell <landonjpowell@airmail.cc>
2016-04-07 11:50:04 -0400

    Update DeviousYarn.go

--- DeviousYarn.go
+++ DeviousYarn.go
@@ -8,7 +8,7 @@ import (
 )
 
 func tokenizer(plaintext string) [][]string {
-    strings := "'[^']+'"
+    strings := "'[^']+'|\"[^\n]+"
     brackets:= "[\\[\\](){}]"
     names   := "[\\w:-@^-`~\\|*-/!-&]+"
 
