9ae541e1b1f129c8bf881238b00f43a579c98e6b
Landon Powell <landonjpowell@airmail.cc>
2016-06-05 13:32:27 -0400

    More Things

--- DeviousYarn.go
+++ DeviousYarn.go
@@ -7,16 +7,25 @@ import (
     "regexp"
 )
 
-func tokenizer(plaintext string) [][]string {
+func tokenizer(plaintext string) []string {
     strings     := "'(\\\\'|[^'])+'|\"[^\n]+"
     brackets    := "[\\[\\](){}]"
     names       := "[\\w:-@^-`~\\|*-/!-&]+"
 
     tokens  := regexp.MustCompile(strings+"|"+brackets+"|"+names)
-    return tokens.FindAllStringSubmatch(plaintext, -1)
+    return tokens.FindAllString(plaintext, -1)
 }
 
-func nester(tokenList [][]string) {
+
+type tree struct { // Tree of processes. It can also be a value.
+    value   string  // If it's a value.
+    subtree []tree  // If it's a bunch of subprocesses.
+}
+
+func nester(tokenList []string) tree {
+    for _, token := range tokenList {
+        token
+    }
 }
 
 func main() {
@@ -24,7 +33,7 @@ func main() {
     reader := bufio.NewReader(os.Stdin)
 
     var input string
-    for input != "kill\n" && input != "exit\n" {
+    for input != "kill\n" {
 
         fmt.Println(" -input- ")
         input, _ = reader.ReadString('\n')
