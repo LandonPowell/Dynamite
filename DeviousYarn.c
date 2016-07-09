eed1135592f03c99e989028b08e03a71680cd334
xultra <landonjpowell@airmail.cc>
2016-07-09 03:44:05 +0000

    Started work on the C version

---
+++ DeviousYarn.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+char** tokenizer(char plaintext []) {
+    char* tokens [] = {};
+    char  word;
+
+    int index;
+    for (index = 0; plaintext[index] != 0; index++) {
+        // printf("%c \n", plaintext[index] ); 
+    }
+
+    char** tokenList = tokens;
+    return tokenList;
+}
+
+main() {
+    char input[999];
+    while (input != "kill\n") {
+        printf(" -input- \n");
+        fgets(input, 100, stdin);
+
+        printf(" -output- \n");
+
+        tokenizer(input);
+
+        printf("%s", input);
+    }
+    
+    printf(" Thanks for using DeviousYarn~! ");
+}
